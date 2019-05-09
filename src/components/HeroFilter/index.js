1b5d56940faa813ef8dd19c540b2f261f3064dff
Luis Delgado <memelogias@gmail.com>
2019-05-09 12:22:08 -0400

    Add state

--- src/components/HeroFilter/index.js
+++ src/components/HeroFilter/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const HeroFilter = ({ placeholder }) => (
+const HeroFilter = ({ placeholder, handleInputChange, value }) => (
   <div className="search-input">
-    <input type="text" placeholder={placeholder} />
+    <input
+      type="text"
+      placeholder={placeholder}
+      onChange={handleInputChange}
+      value={value}
+    />
   </div>
 )
 
--- src/containers/HeroContainer/index.js
+++ src/containers/HeroContainer/index.js
@@ -7,20 +7,56 @@ const heroes = [
   { name: 'Aragorn', race: 'Human', age: '120', weapon: 'Sword ⚔' },
   { name: 'Legolas', race: 'Elf', age: '200', weapon: 'Bow 🏹' },
   { name: 'Gimli', race: 'Dwarf', age: '139', weapon: 'Axe ⚒' },
-  { name: 'Frodo', race: 'Hobbit', age: '33', weapon: 'Dagger 🗡' }
+  { name: 'Frodo', race: 'Hobbit', age: '33', weapon: 'Dagger 🗡' },
+  { name: 'Bilbo', race: 'Hobbit', age: '200', weapon: 'Dagger 🗡' },
 ]
 
 export default class HeroContainer extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      filterText: ''
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange (event) {
+    this.setState({
+      filterText: event.target.value
+    })
+  }
+  
   render() {
+    const { filterText } = this.state
+
+    let filteredArray = heroes
+
+    if (filterText) {
+      filteredArray = filteredArray.filter(hero => {
+        return (
+          hero.name.toLowerCase().includes(filterText.toLowerCase()) ||
+          hero.race.toLowerCase().includes(filterText.toLowerCase())
+        )
+      })
+    }
+
     return (
       <div className="App">
         <div className="index">
           <h2>Fellowship of the Ring</h2>
 
           <div className="container">
-            <HeroFilter placeholder='Search the hero' />
+            <HeroFilter
+              placeholder='Search the hero'
+              handleInputChange={this.handleInputChange}
+              value={filterText}
+            />
 
-            <HeroTable heroes={heroes} />
+            {filteredArray.length === 0 && <div>No heroes...</div>}
+
+            {filteredArray.length > 0 && <HeroTable heroes={filteredArray} />}
           </div>
         </div>
       </div>
